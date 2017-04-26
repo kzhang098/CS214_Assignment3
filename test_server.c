4936de1c6668b2104a3060d7365b1f30e0182837
kevinpei <kevinpei@users.noreply.github.com>
2017-04-26 18:33:54 -0400

    Fixed some errors

--- test_server.c
+++ test_server.c
@@ -5,6 +5,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 /*
 *GLOBAL VARIABLES
@@ -55,7 +56,7 @@ char ** tokenizeMessage(char* message) {
 	//Index values and array to store the indices. 
 
 	int funcIndex, fdIndex, pathIndex, flagIndex, bufferIndex, lengthIndex;
-	int * indexArr[7]; 
+	int indexArr[7]; 
 	
 	indexArr[0] = 0;
 	funcIndex = strcspn(message, ","); 
@@ -73,14 +74,14 @@ char ** tokenizeMessage(char* message) {
 
 	//Argument values
 
-	char * functionName = (char*)malloc(sizeof(funcIndex + 1));
-	char * fileDes = (char*)malloc(sizeof(fdIndex + 1)); 
-	char * path = (char*)malloc(sizeof(pathIndex + 1)); 
-	char * flags = (char*)malloc(sizeof(flagIndex + 1));
-	char * buffer = (char*)malloc(sizeof(bufferIndex + 1));
-	char * length = (char*)malloc(sizeof(lengthindex + 1)); 
+	char * functionName = (char*)malloc(funcIndex + 1);
+	char * fileDes = (char*)malloc(fdIndex + 1); 
+	char * path = (char*)malloc(pathIndex + 1); 
+	char * flags = (char*)malloc(flagIndex + 1);
+	char * buffer = (char*)malloc(bufferIndex + 1);
+	char * length = (char*)malloc(lengthIndex + 1); 
 	
-	int i
+	int i;
 	int value;  
 
 
@@ -88,32 +89,32 @@ char ** tokenizeMessage(char* message) {
 		if(indexArr[i+1] - indexArr[i] != 1) {
 			switch(i) {
 				case 0:
-					strncpy(functionName, message[0], indexArr[1] - indexArr[0]);
+					strncpy(functionName, &message[0], indexArr[1] - indexArr[0]);
 					strcat(functionName, "\0");  
 					break; 		
 				case 1:
 					value = indexArr[1] + 1;
-					strncpy(fileDes, message[value], indexArr[2] - indexArr[1]);
+					strncpy(fileDes, &message[value], indexArr[2] - indexArr[1]);
 					strcat(fileDes, "\0");
 					break;  
 				case 2:
 					value = indexArr[2] + 1;
-					strncpy(path, message[value], indexArr[3] - indexArr[2]);
+					strncpy(path, &message[value], indexArr[3] - indexArr[2]);
 					strcat(path, "\0");
 					break; 
 				case 3:
 					value = indexArr[3] + 1;
-					strncpy(flags, message[value], indexArr[4] - indexArr[3]);
+					strncpy(flags, &message[value], indexArr[4] - indexArr[3]);
 					strcat(flags, "\0");
 					break;
 				case 4:
 					value = indexArr[4] + 1;
-					strncpy(buffer, message[value], indexArr[5] - indexArr[4]);
+					strncpy(buffer, &message[value], indexArr[5] - indexArr[4]);
 					strcat(buffer, "\0");
 					break;
 				case 5: 
 					value = indexArr[5] + 1;
-					strncpy(length, message[value], indexArr[6] - indexArr[5]); 
+					strncpy(length, &message[value], indexArr[6] - indexArr[5]); 
 					strcat(length, "\0");
 					break;
 			}
@@ -163,7 +164,7 @@ char ** tokenizeMessage(char* message) {
 int readFromSocket(int socket, clientInfo * client) {
 
 	//For testing purposes: 
-	printf("This is the buffer received from the client: %s\n", buffer);
+//	printf("This is the buffer received from the client: %s\n", buffer);
 
 	//After which I should be able to parse the data from the packet and store it in the packetData struct. 
 	return 0; 
@@ -176,14 +177,14 @@ int readFromSocket(int socket, clientInfo * client) {
 int runCommands(clientInfo * client) {
 	int * socket = client->socketId; // This is the socket we are using to communicate with the client
 	
-	readFromSocket(socket, client); 
+	readFromSocket(*socket, client); 
 	
 	return 0; 
 }
 
 int main(int argc, char ** argv) {
 
-	int sockfd; //Stands for socket found 
+	int sockfd; //Stands for socket file descriptor
 	int newsockfd;
 	int portNum; 
 	int n; 
@@ -213,13 +214,15 @@ int main(int argc, char ** argv) {
 		printf("Error binding to client");
 		exit(1);
 	} 
-
+	
 	listen(sockfd, 1);
-	int i; 	
+	int i = 0; 	
 	int clientNum = 0;
 	
 	while (1) {
 		while(i < 10) {
+
+			int flag = 1;
 			clilen = sizeof(cli_addr);
 			newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 			
@@ -231,9 +234,10 @@ int main(int argc, char ** argv) {
 				*(cInfo->socketId) = newsockfd; //Stores the socket into the struct. 
 				cInfo->clientId = i; 
 				cInfo->commands = (packetData*)malloc(sizeof(packetData)); 
+				flag = pthread_create(&clients[i], NULL, (void*)runCommands, cInfo); //Starts a new thread with the created struct 
 			}
 			
-			flag = pthread_create(&clients[i], NULL, (void*)runCommands, cInfo);  //Starts a new thread with the created struct 
+
 			
 			if(flag == 1) {
 
